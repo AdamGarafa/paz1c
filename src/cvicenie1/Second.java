17c19d08ebfc84b4efe94fc431420cbd259a1324
Adam <Adam@LAPTOP-7995RAO4>
2023-09-26 13:15:11 +0200

    Pridana modra metoda.

--- src/cvicenie1/Second.java
+++ src/cvicenie1/Second.java
@@ -4,4 +4,8 @@ public class Second {
 	public static void metoda() {
 		System.out.println("Metoda druhej triedy bola spustena.");
 	}
+	
+	public void modra() {
+		System.out.println("Modra metoda druhej triedy.");
+	}
 }
