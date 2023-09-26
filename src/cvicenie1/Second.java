c35c9771a99ceec103e48736f8cee7d559c14da0
Adam <Adam@LAPTOP-7995RAO4>
2023-09-26 13:13:38 +0200

    Pridana cervena metoda.

--- src/cvicenie1/Second.java
+++ src/cvicenie1/Second.java
@@ -4,4 +4,8 @@ public class Second {
 	public static void metoda() {
 		System.out.println("Metoda druhej triedy bola spustena.");
 	}
+	
+	public void cervena() {
+		System.out.println("Cervena metoda druhej triedy.");
+	}
 }
